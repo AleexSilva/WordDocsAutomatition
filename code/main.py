080aef7ba129620c29b0d3b0711f6704ae1657cd
Alex Silva <silva.alexis94@gmail.com>
2024-07-13 12:56:27 -0300

    Add excel file

--- code/main.py
+++ code/main.py
@@ -1,14 +1,28 @@
 import pandas as pd
 from datetime import datetime
 from docxtpl import DocxTempate
-
+import os
 # Global Variables
 
 name = "Alex Silva"
-Phone = "+1 547822516"
+phone = "+1 547822516"
 email = "Alex.silva94@gmail.com"
 date =  datetime.today().strftime('%Y-%m-%d')
 
+DICT = {'name':name,'phone':phone,'email':email,'date':date}
+
+
+base_path = 'input'
+file_names = ['templates.docx','students.xls']
+
+
 #Iniciate Doc Template
 
-doc = DocxTempate('template.docx')
+file_path1 = os.path.join(base_path, file_names[0])
+doc = DocxTempate(file_path1)
+
+
+# Read Excel file
+file_path2 = os.path.join(base_path, file_names[1])
+df = pd.read_excel(file_path2)
+df
